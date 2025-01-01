b7a6147c0814d7e7abc710219ed5de070210a4d4
flotob <florian@glatz-mahlberg.de>
2025-01-02 00:41:48 +0100

    add pwa mode

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'portfolio-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/assets/css/main.css',
+  '/assets/js/navigation.js',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+  '/favicon.ico'
+];
+
+// Install event - cache basic assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch event - serve from cache, falling back to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request)
+          .then((response) => {
+            // Cache new successful responses
+            if (response && response.status === 200 && response.type === 'basic') {
+              const responseToCache = response.clone();
+              caches.open(CACHE_NAME)
+                .then((cache) => {
+                  cache.put(event.request, responseToCache);
+                });
+            }
+            return response;
+          });
+      })
+  );
+}); 
