68d17b957d0d723a0c1386eda7be5aa48ed819b3
flotob <florian@glatz-mahlberg.de>
2025-01-03 13:23:08 +0100

    improve

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -136,10 +136,7 @@ class Slideshow {
                     </div>
                     <div class="slide-footer-right">
                         <a href="${this.currentProject.url}" target="_blank">
-                            Visit Website
-                            <svg width="16" height="16" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
-                            </svg>
+                            🔗 Visit Website
                         </a>
                     </div>
                 </div>
